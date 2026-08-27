a96b804a74383714d6b8c370213953b07a4acd4b
olegrubvv-pixel <olegrubtsovvv@gmail.com>
2026-08-27 23:28:16 +0900

    Update plugin versions in build.gradle.kts
    Updated plugin versions for Android application and Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.numbergames.app"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.numbergames.app"
-        minSdk = 23
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            applicationIdSuffix = ".debug"
-            versionNameSuffix = "-debug"
-        }
-    }
-
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
